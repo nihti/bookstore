89fe9896a3529868775aca390d689635c510308b
nihti <nihti.konemie@gmail.com>
2020-09-14 13:02:46 +0300

    Bookstore week two, project structure added

---
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -0,0 +1,23 @@
+package hh.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+
+@Controller
+public class BookController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}

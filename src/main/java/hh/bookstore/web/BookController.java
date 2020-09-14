3702a1f2f2b750c477676b7402e018b3a7176fbc
nihti <nihti.konemie@gmail.com>
2020-09-14 14:18:57 +0300

    Bookstore week three, Chapter 3 assigment 2.

--- src/main/java/hh/bookstore/web/BookController.java
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package hh.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +12,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import hh.bookstore.domain.BookRepository;
+
 import java.util.ArrayList;
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository bookrepo;
 
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/booklist")
+	public String listBooks(Model model) {
+		model.addAttribute("books", bookrepo.findAll());
+		return "booklist";
 	}
 }
